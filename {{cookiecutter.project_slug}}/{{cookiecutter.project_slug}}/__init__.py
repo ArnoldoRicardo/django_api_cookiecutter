85cfa7501fe3c09d0412e15f9b341a32e234345f
ArnoldoRicardo <anyback@gmail.com>
2021-03-11 21:18:44 -0600

    code

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,7 @@
-"""Top-level package for {{ cookiecutter.project_name }}."""
-
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = '{{ cookiecutter.email }}'
-__version__ = '{{ cookiecutter.version }}'
+__version__ = "0.1.0"
+__version_info__ = tuple(
+    [
+        int(num) if num.isdigit() else num
+        for num in __version__.replace("-", ".", 1).split(".")
+    ]
+)
